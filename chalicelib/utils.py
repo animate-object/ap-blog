8bd4a1f5cab267d8d21cc47147f44c822794299f
alex prinsen <alexander.f.prinsen@gmail.com>
2018-04-11 20:35:38 -0500

    some exception handling

--- chalicelib/utils.py
+++ chalicelib/utils.py
@@ -1,1 +1,12 @@
+from functools import wraps
+from traceback import print_exc
 
+def handle_errors(f):
+    @wraps(f)
+    def handle_errors_wrapper(*args, **kwargs):
+        try:
+            return f(*args, **kwargs)
+        except:
+            print_exc()
+            raise
+    return handle_errors_wrapper
